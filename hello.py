d437da891920f6f5af2bb2146d1c812cad714dd4
Soonyen Ju <soonyenju@foxmail.com>
2019-05-07 14:24:02 +0800

    init

---
+++ hello.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, abort, redirect, url_for, render_template
+from jinja2 import Template
+from markdown import markdown
+
+app = Flask(__name__, template_folder = "./public")
+app.config["DEBUG"] = True
+
+template = Template("Hello {{name}}")
+
+@app.route('/')
+def hello_world():
+    Dict = {
+        "content": "jojo",
+        "items": [
+            {
+                "href": "baidu",
+                "caption": "search engine"
+            }
+        ]
+    }
+    return render_template("index.html", **Dict)
+
+@app.route('/code/')
+def mycode():
+    return template.render(name = "songyan")
+
+@app.route('/md/')
+def parse_md():
+    markDown_text = "good"
+    with open("text.md", 'r', encoding='utf-8') as f:
+        markDown_text = f.read()
+    html_text = markdown(markDown_text, output_format = 'html')
+    return html_text
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=3000)
+
+'''
+if no main(), use following code snippet:
+set FLASK_APP=hello.py
+python -m flask run
+'''
